511be14e1b21072c561a989192b18bb679e52b28
theschmocker <jacobcs1996@gmail.com>
2019-09-21 19:37:50 -0500

    swap episode api url in production

--- frontend/src/app/episode.service.ts
+++ frontend/src/app/episode.service.ts
@@ -4,11 +4,15 @@ import { HttpClient } from "@angular/common/http";
 import { Episode } from '../../../shared/episode';
 import { Observable } from 'rxjs';
 
+import { environment } from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EpisodeService {
-  private baseUrl = 'http://localhost:3000/episode'
+  private baseUrl = environment.production 
+    ? 'http://netflix-shuffle.jacobschmocker.com/episode'
+    : 'http://localhost:3000/episode'
   private episodes$ = this.http.get<Episode[]>(this.baseUrl)
   
   constructor(private http: HttpClient) { }
