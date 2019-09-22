97d3a3743751606b7a8b657ad10316fd6c759636
theschmocker <jacobcs1996@gmail.com>
2019-09-21 19:29:16 -0500

    change cors allow setting depending on environment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import {config} from 'dotenv';
+config();
+
+const origin = process.env.ENVIRONMENT === 'production'
+  ? 'https://netflix-shuffle.netlify.com'
+  : 'http://localhost';
+
+  console.log(origin);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://netflix-shuffle.netlify.com',
+    origin,
   })
   await app.listen(3000);
 }
